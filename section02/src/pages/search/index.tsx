2c84fb12ba01b866069bb18c37882c60deaa9bb2
chaewon <devchae10@gmail.com>
2026-03-30 23:03:08 +0900

    init: onebite-next

---
+++ section02/src/pages/search/index.tsx
@@ -0,0 +1,45 @@
+import SearchableLayout from "@/components/searchable-layout";
+import { ReactNode, useEffect, useState } from "react";
+import BookItem from "@/components/book-item";
+import fetchBooks from "@/lib/fetch-books";
+import { useRouter } from "next/router";
+import { BookData } from "@/types";
+
+// export const getStaticProps = async (context: GetStaticPropsContext) => {
+// 	const q = context.query.q;
+// 	const books = await fetchBooks(q as string);
+
+// 	return {
+// 		props: { books },
+// 	};
+// };
+
+export default function Page() {
+	const [books, setBoooks] = useState<BookData[]>([]);
+
+	const router = useRouter();
+	const q = router.query.q;
+
+	const fetchSearchResult = async () => {
+		const data = await fetchBooks(q as string);
+		setBoooks(data);
+	};
+
+	useEffect(() => {
+		if (q) {
+			fetchSearchResult();
+		}
+	}, [q]);
+
+	return (
+		<div>
+			{books.map((book) => (
+				<BookItem key={book.id} {...book} />
+			))}
+		</div>
+	);
+}
+
+Page.getLayout = (page: ReactNode) => {
+	return <SearchableLayout>{page}</SearchableLayout>;
+};
